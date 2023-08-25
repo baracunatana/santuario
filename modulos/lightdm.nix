e77299a5b6f82d2a2bd791e7cd83e09c88f5dc8f
Juan E. Gómez-Morantes <juanerasmoe@gmail.com>
2023-08-25 08:57:30 -0500

    Cambio de imagen de fondo a lightdm (con slick)

--- modulos/lightdm.nix
+++ modulos/lightdm.nix
@@ -1,8 +1,17 @@
 {pkgs, ...}: {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.displayManager.lightdm.greeters.slick.enable = true;
+  services.xserver.displayManager.lightdm = {
+    enable = true;
+    greeters.slick = {
+      enable = true;
+      extraConfig = ''
+        background=/etc/lightdm/jardin.jpg
+      '';
+    };
+  };
+
+  environment.etc."lightdm/jardin.jpg".source = ../wallpapers/jardin.jpg;
 
   # Configurar teclado en X11
   services.xserver = {

762a609b827ab32e154039007e9979a786797622
Juan E. Gómez-Morantes <juanerasmoe@gmail.com>
2023-08-25 09:24:04 -0500

    Git ahora puede enviar archivos grandes

--- home/aplicaciones/git.nix
+++ home/aplicaciones/git.nix
@@ -9,6 +9,12 @@
       enable = true;
       userName = "Juan E. Gómez-Morantes";
       userEmail = "juanerasmoe@gmail.com";
+      # Para commits de archivos grandes. Ver https://stackoverflow.com/questions/2702731/git-fails-when-pushing-commit-to-github
+      extraConfig = {
+        http = {
+          postBuffer  = "524288000";
+        };
+      };
     };
   };
 }

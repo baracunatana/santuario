4c9eb882bf0fb881eb039bdc4e7ae4371bea2dbf
Juan E. Gómez-Morantes <juanerasmoe@gmail.com>
2023-09-26 08:15:47 -0500

    Cache de credenciales de git

--- home/aplicaciones/git.nix
+++ home/aplicaciones/git.nix
@@ -9,13 +9,16 @@
       enable = true;
       userName = "Juan E. Gómez-Morantes";
       userEmail = "juanerasmoe@gmail.com";
-      # Para commits de archivos grandes. Ver https://stackoverflow.com/questions/2702731/git-fails-when-pushing-commit-to-github
       extraConfig = {
+        # Para commits de archivos grandes. Ver https://stackoverflow.com/questions/2702731/git-fails-when-pushing-commit-to-github
         http = {
           postBuffer  = "500M";
           maxRequestBuffer = "100M";
         };
         core.compression = "0";
+
+        # Para guardar credenciales en memoria por 1 hora
+        credential.helper = "cache --timeout=3600";
       };
     };
   };

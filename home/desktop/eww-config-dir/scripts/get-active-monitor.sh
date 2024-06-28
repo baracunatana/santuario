a0d579c636e7a0e9bb89b487376d5f03ca469da3
Juan E. Gómez-Morantes <juanerasmoe@gmail.com>
2024-06-28 11:56:25 -0500

    eww > Correcciones para 2405

--- home/desktop/eww-config-dir/scripts/get-active-monitor.sh
+++ home/desktop/eww-config-dir/scripts/get-active-monitor.sh
@@ -9,4 +9,4 @@ process() {
 
 hyprctl monitors -j | jq '.[] | select(.focused) | .id'
 
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do process "$line" ; done
+socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do process "$line" ; done

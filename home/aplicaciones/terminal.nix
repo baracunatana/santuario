3e8eff62f8573ea3e8f986928ca8cdd9d3dd5537
Juan E. Gómez-Morantes <juanerasmoe@gmail.com>
2024-06-28 19:35:51 -0500

    alacritty > Configuración con hm y tema gráfico

--- home/aplicaciones/terminal.nix
+++ home/aplicaciones/terminal.nix
@@ -2,20 +2,31 @@
   config,
   pkgs,
   ... }:
-
 {
   home.packages = with pkgs; [
     xterm
   ];
-    
+  
   programs = {
     alacritty = {
       enable = true;
       settings = {
+	      import = ["~/.config/alacritty/themes/themes/nord.toml"];
         font.normal = {
-	  family = "Inconsolata LGC";
-	};
+	        family = "Inconsolata LGC";
+	      };
       };
+    };
+  };
+
+  # A continuación se clona el repo de temas gráficos para alacritty
+  home.file = {
+    ".config/alacritty/themes".source = pkgs.fetchFromGitHub {
+      owner = "alacritty";
+      repo = "alacritty-theme";
+      rev = "a4041ae";
+      # Se consulta con nix-prefetch-github alacritty alacritty-theme
+      sha256 = "A5Xlu6kqB04pbBWMi2eL+pp6dYi4MzgZdNVKztkJhcg=";
     };
   };
 }

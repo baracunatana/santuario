26e27430f5f3f03c33775daa2edbc230189dfdf8
Juan E. Gómez-Morantes <juanerasmoe@gmail.com>
2024-08-21 16:51:44 -0500

    Agrega spotifyd

--- home/aplicaciones/media.nix
+++ home/aplicaciones/media.nix
@@ -7,8 +7,21 @@
   home.packages = with pkgs; [
     pinta
     spotify
+    spotifyd
   ];
-    
+   
+  home.file.".config/spotifyd/spotifyd.conf".text =''
+    [global]
+    username = "juanerasmoe@gmail.com"
+    devicename = "shura"
+    # password_cmd = "gpg2 -q --for-your-eyes-only --no-tty -d ~/.config/spotifyd.gpg"
+    password = "Al8DV15Sx61CTrtu1Ehs"
+    device_name = "shura"
+    bitrate = 320
+    volume_normalisation = true
+    device_type = "speaker"
+    '';
+ 
   programs = {
     mpv = {
       enable = true;

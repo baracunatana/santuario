9823c1cca99661e1b693e8f03ce1123a58e9a8d7
Juan E. Gómez-Morantes <juanerasmoe@gmail.com>
2025-03-23 09:28:46 -0500

    Correr spotifyd como servicio

--- home/aplicaciones/media.nix
+++ home/aplicaciones/media.nix
@@ -1,27 +1,39 @@
 { 
   config,
   pkgs,
+  pkgs-unstable,
   ... }:
 
 {
   home.packages = with pkgs; [
     pinta
     spotify
-    spotifyd
+    pkgs-unstable.spotifyd
   ];
    
   home.file.".config/spotifyd/spotifyd.conf".text =''
     [global]
-    username = "juanerasmoe@gmail.com"
-    devicename = "shura"
-    # password_cmd = "gpg2 -q --for-your-eyes-only --no-tty -d ~/.config/spotifyd.gpg"
-    password = "Al8DV15Sx61CTrtu1Ehs"
-    device_name = "shura"
-    bitrate = 320
+    #username = "juanerasmoe@gmail.com"
+    devicename = "shion"
+    #password_cmd = "gpg2 -q --for-your-eyes-only --no-tty -d ~/.config/spotifyd.gpg"
+    #bitrate = 320
     volume_normalisation = true
     device_type = "speaker"
     '';
- 
+
+  services = { 
+    spotifyd = {
+      enable = true;
+      package = pkgs-unstable.spotifyd;
+      settings = {
+        global = {
+          device_name = "nix";
+          bitrate = 320;
+          volume_normalisation = true;
+        };
+      };
+    };
+  };
   programs = {
     mpv = {
       enable = true;

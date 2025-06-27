7093743a8d93fe7fa874ec97086e6124cbd4505d
Juan E. Gómez-Morantes <juanerasmoe@gmail.com>
2025-06-27 09:10:29 -0500

    Eliminar qutebrowser

--- home/aplicaciones/navegadores.nix
+++ home/aplicaciones/navegadores.nix
@@ -12,68 +12,6 @@
   ];
 
   programs = {
-    qutebrowser = {
-      enable = true;
-      package = pkgs.qutebrowser;
-      loadAutoconfig = false;
-      
-      settings = {
-        auto_save.session = true;
-	      content.pdfjs = true;
-      };
-
-      keyBindings = {
-        normal = {
-	        # Movimiento en historia de navegación
-          "<Ctrl+j>" = "back";
-          "<Ctrl+k>" = "forward";
-	        # Movimiento entre pestañas
-          "<Ctrl+h>" = "tab-prev";
-          "<Ctrl+l>" = "tab-next";
-	        # Gestión de pestañas
-          "<Space>bn" = "tab-give";
-          "<Space>be" = "tab-close";
-          # Apertura o ejecución externa
-          "<Space>ov" = "int links spawn mpv {hint-url}";
-          "<Space>of" = "pawn firefox {url}";
-        };
-        
-        command = {
-          "<Ctrl+j>" = "completion-item-focus next";
-          "<Ctrl+k>" = "completion-item-focus prev";
-	      };
-      };
-
-      extraConfig = "config.unbind('H')\nconfig.unbind('L')";
-
-      aliases = {
-        q = "close";
-        pensaSis = "open -t https://campusvirtuallms.javeriana.edu.co/d2l/home/262685";
-        proyEmp = "open -t https://campusvirtuallms.javeriana.edu.co/d2l/home/262887";
-      };
-      
-      searchEngines = {
-        # Búsquedas generales
-        DEFAULT = "https://duckduckgo.com/?q={}";
-        go = "https://www.google.com/search?hl=en&q={}";
-        am = "https://www.amazon.com/s?k={}";
-        # Bibliografía y libros
-        cr = "https://search.crossref.org/?from_ui=&q={}";
-        gs = "https://scholar.google.com/scholar?q={}"; 
-        gr = "https://www.goodreads.com/search?query={}";
-        lgnf = "http://libgen.is/search.php?req={}"; 
-        lgf = "https://libgen.is/fiction/?q={}"; 
-        sc = "https://www.scimagojr.com/journalsearch.php?q={}";
-        # NixOS
-        np = "https://search.nixos.org/packages?query={}";
-        no = "https://search.nixos.org/options?query={}";
-	      nh = "https://home-manager-options.extranix.com/?query={}";
-        # Redes sociales
-        re = "https://www.reddit.com/r/{}"; 
-        yt = "https://www.youtube.com/results?search_query={}";
-      };
-    };
-
     firefox = {
       enable = true;
       package = pkgs.firefox;
@@ -155,16 +93,6 @@
       extensions = [
         { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
       ];
-    };
-  };
-
-  xdg.mimeApps = {
-    enable = true;
-    
-    defaultApplications = {
-      "text/html" = "org.qutebrowser.qutebrowser.desktop";
-      "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
-      "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
     };
   };
 }
